5d21de0205bbf350d7307b15ef66a06cd2954c88
askalski <andrzej.j.skalski@gmail.com>
2017-03-20 23:27:36 +0100

    help fix me imports!

--- src/bsshc.rs
+++ src/bsshc.rs
@@ -1,17 +1,28 @@
-// use std::io::Write;
-use std::io::Read;
-use std::net::TcpListener;
-use std::thread;
+use std::{env, error};
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+use version;
+
+const HOST: &'static str = "127.0.0.1:5555";
+
+fn send_one_command(command: &Vec<u8>) -> Result<String, Box<error::Error + Send + Sync>> {
+
+    let mut stream = try!(TcpStream::connect(HOST));
+    try!(stream.write_all(&command));
+
+    let mut response = String::new();
+    let mut limited = stream.take(1024);
+    try!(limited.read_to_string(&mut response));
+
+    Ok(response)
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:5555").unwrap();
-    println!("listening started, ready to accept");
-    for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            let mut inp: Vec<u8> = Vec::new();
-            stream.read_to_end(&mut inp);
-            println!("{:?}", inp);
-        });
+    let hello : Vec<u8> = version::get_version_byte_string() + b"\r\n".to_vec();
+
+    match send_one_command(&hello) {
+        Ok(response) => println!("{}", response),
+        Err(err) => println!("An error occurred: {}", err),
     }
 }
