d83be4714cff348f6e2a45d374fc87e003c2bbb3
askalski <andrzej.j.skalski@gmail.com>
2017-03-22 23:57:49 +0100

    yeeeah

--- src/msgs.rs
+++ src/msgs.rs
@@ -2,9 +2,8 @@ use std::{env, error};
 use std::net::TcpStream;
 use std::io::Read;
 
+use regex::bytes::Regex;
 use bssh_err;
-
-use regex::Regex;
 
 const buffer_length : usize = 255;
 
@@ -12,15 +11,15 @@ pub fn read_welcome_string(mut stream : TcpStream, allow_comments : bool) -> Res
     let mut buf : [u8; buffer_length];
     let mut res : Vec<String> = Vec::new();
 
-    let eol = Regex::new(r"\r\n");
+    let eol = Regex::new(r"\r\n").unwrap();
 
     loop {
         buf = [0 as u8; buffer_length];
         stream.read_exact(&mut buf);
-        // let line = match buf.windows(2).position(|&x| x == ['\r' as u8, '\n' as u8]) {
-        //     None => panic!(bssh_err::BSSH_ERR_NO_DELIMITER_FOUND),
-        //     Some(idx) => buf[..idx].to_vec()
-        // };
+        let line = match eol.find(&buf) {
+            None => panic!(bssh_err::BSSH_ERR_NO_DELIMITER_FOUND),
+            Some(idx) => buf[..idx.start()].to_vec()
+        };
 
 
 
