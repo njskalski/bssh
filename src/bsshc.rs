ab9f905df97d97eae0ef45b8e9cfcd2cc1ca9ab6
askalski <andrzej.j.skalski@gmail.com>
2017-03-22 22:10:39 +0100

    still playing

--- src/bsshc.rs
+++ src/bsshc.rs
@@ -1,28 +1,30 @@
 use std::{env, error};
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::net::Shutdown;
 
 mod version;
 
 const HOST: &'static str = "127.0.0.1:5555";
 
-fn send_one_command(command: &Vec<u8>) -> Result<String, Box<error::Error + Send + Sync>> {
+fn send_one_command(command: &Vec<u8>) -> Result<(), Box<error::Error + Send + Sync>> {
 
     let mut stream = try!(TcpStream::connect(HOST));
     try!(stream.write_all(&command));
 
-    let mut response = String::new();
-    let mut limited = stream.take(1024);
-    try!(limited.read_to_string(&mut response));
+    // let mut response = String::new();
+    // stream.read_to_string(&mut response);
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+    // println!("{:?}", response);
 
-    Ok(response)
+    Ok(())
 }
 
 fn main() {
     let hello : Vec<u8> = [version::get_version_byte_string(),  b"\r\n".to_vec()].concat();
 
     match send_one_command(&hello) {
-        Ok(response) => println!("{}", response),
+        Ok(_) => println!("ok"),
         Err(err) => println!("An error occurred: {}", err),
     }
 }
--- src/bsshd.rs
+++ src/bsshd.rs
@@ -1,19 +1,52 @@
-
+use std::io::Write;
 use std::io::Read;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::net::Shutdown;
 use std::thread;
 
+mod version;
 
+const HOST: &'static str = "127.0.0.1:5555";
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf;
+    loop {
+        // clear out the buffer so we don't send garbage
+        buf = [0; 512];
+        let num = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    // we've got an EOF
+                    println!("eof");
+                    break;
+                }
+                m
+            },
+        };
+
+        let mut vbuf = buf.to_vec();
+        vbuf.truncate(num);
+        println!("got {:?}: {:?}", num, vbuf);
+
+        match stream.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:5555").unwrap();
-    println!("listening started, ready to accept");
+    let listener = TcpListener::bind(HOST).unwrap();
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            let mut inp: Vec<u8> = Vec::new();
-            stream.read_to_end(&mut inp);
-            println!("{:?}", inp);
-        });
+        match stream {
+            Err(e) => { println!("failed: {}", e) }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+        }
     }
 }
